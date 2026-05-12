3a77add7bb98d2f59c0f872e2545b98673e51f46
Eduard Remets <eduard.remets@techno.ee>
2026-05-12 11:40:43 +0300

    add dockerfile

--- server.mjs
+++ server.mjs
@@ -4,6 +4,6 @@ const server = createServer((req, res) => {
   res.end('Hello World!\n');
 });
 
-server.listen(3000, '127.0.0.1', () => {
-  console.log('Listening on 127.0.0.1:3000');
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Listening on 0.0.0.0:3000');
 });
